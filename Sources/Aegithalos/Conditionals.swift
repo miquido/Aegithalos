bffad76000e40d963792d67c8272a81f5041da0b
Kacper Kaliński <kacper.kalinski@miquido.com>
2020-07-03 10:47:04 +0200

    [IOST-30] adding documentation

--- Sources/Aegithalos/Conditionals.swift
+++ Sources/Aegithalos/Conditionals.swift
@@ -1,3 +1,8 @@
+/// Conditionally execute one of two functions and get result. Both functions have to return same type.
+/// - parameter condition: Condition checked to determine which function will be selected.
+/// - parameter then: Function executed when condition evaluates to true.
+/// - parameter else: Function executed when condition evaluates to false.
+/// - returns: Result of either function.
 @discardableResult public func when<T>(
   _ condition: @autoclosure () -> Bool,
   then: () throws -> T,
@@ -11,6 +16,9 @@
   }
 }
 
+/// Conditionally execute  function.
+/// - parameter condition: Condition checked to determine if function will be executed.
+/// - parameter then: Function executed when condition evaluates to true.
 public func when(
   _ condition: @autoclosure () -> Bool,
   then: () throws -> Void
@@ -23,6 +31,11 @@ public func when(
   }
 }
 
+/// Conditionally execute one of two functions and get result. Both functions have to return same type.
+/// - parameter condition: condition checked to determine which function will be selected.
+/// - parameter then: Function executed when condition evaluates to false.
+/// - parameter else: Function executed when condition evaluates to true.
+/// - returns: Result of either function.
 @discardableResult public func whenNot<T>(
   _ condition: @autoclosure () -> Bool,
   then: () throws -> T,
@@ -36,6 +49,9 @@ public func when(
   }
 }
 
+/// Conditionally execute  function.
+/// - parameter condition: Condition checked to determine if function will be executed.
+/// - parameter then: Function executed when condition evaluates to false.
 public func whenNot(
   _ condition: @autoclosure () -> Bool,
   then: () throws -> Void
@@ -49,6 +65,10 @@ public func whenNot(
 }
 
 public extension Bool {
+  
+  /// Conditionally execute  function if true.
+  /// - parameter closure: Function executed when true
+  /// - returns: .Self
   @discardableResult func whenTrue(_ closure: () throws -> Void) rethrows -> Self {
     switch self {
     case true:
@@ -59,6 +79,9 @@ public extension Bool {
     return self
   }
   
+  /// Conditionally execute  function if false.
+  /// - parameter closure: Function executed when false
+  /// - returns: .Self
   @discardableResult func whenFalse(_ closure: () throws -> Void) rethrows -> Self {
     switch self {
     case true:
@@ -71,6 +94,10 @@ public extension Bool {
 }
 
 public extension Optional {
+  
+  /// Conditionally execute  function if wrapped value is present.
+  /// - parameter closure: Function executed with access to wrapped value if any.
+  /// - returns: .Self
   @discardableResult func whenSome(_ closure: (Wrapped) throws -> Void) rethrows -> Self {
     switch self {
     case let .some(wrapped):
@@ -81,6 +108,9 @@ public extension Optional {
     return self
   }
   
+  /// Conditionally execute  function if wrapped value is not present.
+  /// - parameter closure: Function executed if there is no value.
+  /// - returns: .Self
   @discardableResult func whenNone(_ closure: () throws -> Void) rethrows -> Self {
     switch self {
     case .some:
@@ -93,6 +123,10 @@ public extension Optional {
 }
 
 public extension Result {
+  
+  /// Conditionally execute  function on success.
+  /// - parameter closure: Function executed with access to success value if succeeded.
+  /// - returns: .Self
   @discardableResult func whenSuccess(_ closure: (Success) throws -> Void) rethrows -> Self {
     switch self {
     case let .success(success):
@@ -103,6 +137,9 @@ public extension Result {
     return self
   }
   
+  /// Conditionally execute  function on failure.
+  /// - parameter closure: Function executed with access to error if failed.
+  /// - returns: .Self
   @discardableResult func whenFailure(_ closure: (Failure) throws -> Void) rethrows -> Self {
     switch self {
     case .success:
--- Sources/Aegithalos/Setup.swift
+++ Sources/Aegithalos/Setup.swift
@@ -1,52 +1,104 @@
+/// Wrapper for a function allowing mutation of passed subject.
+/// Can be composed with other `Setup`  instances and suitable functions.
+/// Allows contramapping for more extensible composition.
+/// - note: `Setup` behaves differently on value and reference types.
+/// Depending on exact `Subject` type it requires reference for class instances,
+/// or mutable reference for enums and structs. This allows to wrap mutating functions
+/// operating on class instances without inout requirement. For value types it is required to
+/// have inout argument.
+/// - note: You can provide fluent API for given `Subject` by preparing extension for `Setup`
+/// with given `Subject` constraint and using `compose` method.
 public struct Setup<Subject> {
   
+  /// Wrapped function.
   @usableFromInline internal let setup: (inout Subject) -> Void
   
+  /// Internal initializer for value types.
   @usableFromInline internal init(setup: @escaping (inout Subject) -> Void) {
     self.setup = setup
   }
-  
-  public static func of(
+}
+
+// MARK: - Value types
+
+public extension Setup {
+
+  /// Type specifying instance of `Setup`. Does not apply any mutation by default allowing further composition.
+  /// - parameter setup: Mutation function wrapped by this `Setup`. Empty function by default.
+  /// - note: It is preffered to prepare `Setup` and specify `Subject` type by using this function
+  /// instead of explicit type specification. `Setup.of(SomeType.self)` is preffered over `Setup<SomeType>`.
+  static func of(
     _: Subject.Type,
     _ setup: @escaping (inout Subject) -> Void = { _ in }
   ) -> Setup {
     .init(setup: setup)
   }
   
-  @inlinable public func callAsFunction(appliedOn subject: inout Subject) -> Subject {
+  /// Call wrapped function on given `Subject` instance.
+  /// - returns: New `Subject` instance that was created after applying mutations on passed subject.
+  /// - warning: If `Subject` is a reference type there will be no copy created
+  /// and all mutations will be applied on passed instance directly.
+  @inlinable func callAsFunction(appliedOn subject: Subject) -> Subject {
+    var subject = subject
     setup(&subject)
     return subject
   }
   
-  @discardableResult @inlinable public func callAsFunction(applyOn subject: inout Subject) -> Self {
+  /// Call wrapped function on given `Subject` instance reference.
+  /// - returns: Same `Subject` instance that was passed as argument.
+  @inlinable func callAsFunction(appliedOn subject: inout Subject) -> Subject {
+    setup(&subject)
+    return subject
+  }
+  
+  /// Call wrapped function on given `Subject` instance reference.
+  /// - returns: Same `Setup` instance.
+  @discardableResult @inlinable func callAsFunction(applyOn subject: inout Subject) -> Self {
     setup(&subject)
     return self
   }
   
-  @inlinable public func composed(with other: @escaping (inout Subject) -> Void) -> Self {
+  /// Extend this `Setup` by composing it with another function.
+  /// - parameter other: Function composed with this `Setup`.
+  /// - returns: New `Setup` instance which composes this one with given function.
+  @inlinable func composed(with other: @escaping (inout Subject) -> Void) -> Self {
     .init { subject in
       self(applyOn: &subject)
       other(&subject)
     }
   }
   
-  @inlinable public func composed(with other: (inout Subject) -> Void...) -> Self {
+  /// Extend this `Setup` by composing it with list of functions.
+  /// - parameter other: List of functions composed with this `Setup`.
+  /// - returns: New `Setup` instance which composes this one with given list of functions.
+  @inlinable func composed(with other: (inout Subject) -> Void...) -> Self {
     .init { subject in
       self(applyOn: &subject)
       other.forEach { $0(&subject) }
     }
   }
   
-  @inlinable public func composed(with other: Self) -> Self {
+  /// Extend this `Setup` by composing it with another `Setup`.
+  /// - parameter other: Other `Setup` composed with this one.
+  /// - returns: New `Setup` instance which composes this one with the other one.
+  @inlinable func composed(with other: Self) -> Self {
     composed(with: other.setup)
   }
   
-  @inlinable public func composed(with other: Self...) -> Self {
+  /// Extend this `Setup` by composing it with list of `Setup` instances.
+  /// - parameter other: List of other `Setup` instances composed with this one.
+  /// - returns: New `Setup` instance which composes this one with list of other `Setup` instances.
+  @inlinable func composed(with other: Self...) -> Self {
     composed { subject in
       other.forEach { $0(applyOn: &subject) }
     }
   }
   
+  /// Map `Subject` type to other type. This allows applying same `Setup` on
+  /// other types that encapsulate instance of original `Subject` type.
+  /// - parameter keyPath: KeyPath used to provide mapping. It has to point to value of
+  /// original `Subject` type inside new `Subject` type.
+  /// - returns: New `Setup` instance applying this one on other `Subject` type.
   @inlinable func contramap<OtherSubject>(
     _ keyPath: WritableKeyPath<OtherSubject, Subject>
   ) -> Setup<OtherSubject> {
@@ -56,26 +108,28 @@ public struct Setup<Subject> {
   }
 }
 
-// MARK: - AnyObject
+// MARK: - Reference types
 
 public extension Setup where Subject: AnyObject {
   
+  /// Internal initializer for reference types.
   @usableFromInline internal init(setup: @escaping (Subject) -> Void) {
     self.setup = { setup($0) }
   }
   
-  @inlinable func callAsFunction(appliedOn subject: Subject) -> Subject {
-    var subject = subject
-    setup(&subject)
-    return subject
-  }
-  
+  /// Call wrapped function on given `Subject` instance reference.
+  /// - returns: Same `Setup` instance.
   @discardableResult @inlinable func callAsFunction(applyOn subject: Subject) -> Self {
     var subject = subject
     setup(&subject)
     return self
   }
   
+  /// Map `Subject` type to other type. This allows applying same `Setup` on
+  /// other types that encapsulate instance of original `Subject` type.
+  /// - parameter transform: Function used to provide mapping. It has to point to value of
+  /// original `Subject` type inside new `Subject` type.
+  /// - returns: New `Setup` instance applying this one on other `Subject` type.
   @inlinable func contramap<OtherSubject>(
     _ transform: @escaping (OtherSubject) -> Subject
   ) -> Setup<OtherSubject>
@@ -86,24 +140,28 @@ public extension Setup where Subject: AnyObject {
   }
 }
 
-// MARK: - function builder
+// MARK: - Function builder
 
 public extension Setup {
   
+  /// Function builder initializer. Allows initialization from list of functions.
   init(@SetupBuilder _ builder: () -> Self) {
     self = builder()
   }
 }
 
+/// Function builder for `Setup`.
 @_functionBuilder
 public enum SetupBuilder {
   
+  /// Function builder for  `Subject` as value type.
   public static func buildBlock<Subject>(
     _ setup: (inout Subject) -> Void...
   ) -> Setup<Subject> {
     Setup { (subject: inout Subject) in setup.forEach { $0(&subject) } }
   }
 
+  /// Function builder for `Subject` as reference type.
   public static func buildBlock<Subject>(
     _ setup: (Subject) -> Void...
   ) -> Setup<Subject>
@@ -112,7 +170,7 @@ public enum SetupBuilder {
   }
 }
 
-// MARK: - experimental
+// MARK: - Operators
 
 infix operator <=<: SetupPrecedence
 
@@ -120,6 +178,8 @@ precedencegroup SetupPrecedence {
   higherThan: SetupComposePrecedence
 }
 
+/// Operator which creates `Setup` instance for given keyPath
+/// as mutation assigning given falue to that keyPath.
 @inlinable public func <=< <Subject, Value>(
   _ lhs: WritableKeyPath<Subject, Value>,
   _ rhs: Value
@@ -129,6 +189,8 @@ precedencegroup SetupPrecedence {
   }
 }
 
+/// Operator which creates  mutating function for given keyPath
+/// as mutation assigning given falue to that keyPath.
 @inlinable public func <=< <Subject, Value>(
   _ lhs: WritableKeyPath<Subject, Value>,
   _ rhs: Value
@@ -138,6 +200,8 @@ precedencegroup SetupPrecedence {
   }
 }
 
+/// Operator which creates  mutating function for given keyPath
+/// as mutation assigning given falue to that keyPath.
 @inlinable public func <=< <Subject, Value>(
   _ lhs: WritableKeyPath<Subject, Value>,
   _ rhs: Value
@@ -155,6 +219,8 @@ precedencegroup SetupComposePrecedence {
   higherThan: AssignmentPrecedence
 }
 
+/// Compose two `Setup` instances.
+/// - returns: New `Setup` instance composing provided two.
 @inlinable public func <> <Subject>(
   _ lhs: Setup<Subject>,
   _ rhs: Setup<Subject>
