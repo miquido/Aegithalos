ebe39f89d42403b8e09c96ed27878a988fc06820
Kacper Kaliński <kacper.kalinski@miquido.com>
2020-07-06 13:25:14 +0200

    [IOST-31] adding UIKit extensions

--- Sources/AegithalosCocoa/UIImage+Setup.swift
+++ Sources/AegithalosCocoa/UIImage+Setup.swift
@@ -8,6 +8,25 @@ public extension Setup where Subject: UIImageView {
   @inlinable func image(_ image: UIImage?) -> Setup {
     composed { imageView in imageView.image = image }
   }
+  
+  @inlinable func image(
+    named imageName: String,
+    from bundle: Bundle? = nil,
+    compatibleWith: UITraitCollection? = nil
+  ) -> Setup {
+    composed { imageView in
+      imageView.image = UIImage(named: imageName, in: bundle, compatibleWith: traitCollection)
+    }
+  }
+  
+  @inlinable func image(
+    symbol symbolName: String,
+    compatibleWith traitCollection: UITraitCollection? = nil
+  ) -> Setup {
+    composed { imageView in
+      imageView.image = UIImage(systemName: symbolName, compatibleWith: traitCollection)
+    }
+  }
 }
 
 #endif
