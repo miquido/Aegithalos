6bcbbf548d94de714631a071d49432fdd469bfae
Kacper Kaliński <kacper.kalinski@miquido.com>
2020-07-10 14:32:13 +0200

    [IOST-31] UIControl add action - memory fix

--- Sources/AegithalosCocoa/ClosureHolder.swift
+++ Sources/AegithalosCocoa/ClosureHolder.swift
@@ -3,13 +3,19 @@ import Foundation
 /// Wrapper for closure that exposes objc selector for invoking it.
 /// Meant to be used for target/action mechanisms from UIKit.
 internal class ClosureHolder {
-    private let closure: (Any?) -> Void
-
-    internal init(_ closure: @escaping (Any?) -> Void) {
-        self.closure = closure
-    }
-
-    @objc internal func invoke(with any: Any) {
-        closure(any)
-    }
+  private let closure: (Any?) -> Void
+  private let cleanup: () -> Void
+  
+  internal init(_ closure: @escaping (Any?) -> Void, cleanup: @escaping () -> Void) {
+    self.closure = closure
+    self.cleanup = cleanup
+  }
+  
+  deinit {
+    cleanup()
+  }
+  
+  @objc internal func invoke(with any: Any) {
+    closure(any)
+  }
 }
--- Sources/AegithalosCocoa/UIControl+ClosureHolder.swift
+++ Sources/AegithalosCocoa/UIControl+ClosureHolder.swift
@@ -10,17 +10,18 @@ internal extension UIControl {
     _ closure: @escaping () -> Void,
     for event: UIControl.Event = .touchUpInside
   ) {
+    // Allocate a single byte for objc associated object key.
+    // We do not store or reuse it since we do not allow removal of those objects. It will live as long as this UIControl.
+    // We don't want to replace previously added closures. However we deallocate that byte on deinit of ClosureHolder
+    let keyPointer = UnsafeMutableRawPointer.allocate(byteCount: 1, alignment: 0)
     // We can't use closures for target/action mechanism so we use wrapper that exposes objc selector.
-    let closureHolder = ClosureHolder { _ in closure() }
+    let closureHolder = ClosureHolder({ _ in closure() }, cleanup: { keyPointer.deallocate() })
     // We have to keep reference to ClosureHolder since `addTarget(_:action:for:)` uses weak reference.
     addTarget(closureHolder, action: #selector(ClosureHolder.invoke(with:)), for: event)
     // To avoid subclassing or external storage we keep that as assocaiated object.
     objc_setAssociatedObject(
       self,
-      // Allocate single byte and get pointer to it. objc associated object requires pointer to a single byte for association.
-      // We do not store or reuse it since we do not allow removal of those objects. It will live as long as this UIControl.
-      // And we don't want to replace previously added closures.
-      UnsafeRawPointer(UnsafeMutableRawPointer.allocate(byteCount: 1, alignment: 0)),
+      UnsafeRawPointer(keyPointer),
       closureHolder,
       .OBJC_ASSOCIATION_RETAIN_NONATOMIC
     )
